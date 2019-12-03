7b3399dafc63185c98edbf2f44c804b7ec5c061f
Rik de Wijn <rdwijn@nl.pamgene.com>
2019-12-03 15:22:00 +0100

    1.4

--- R/main.r
+++ R/main.r
@@ -16,22 +16,23 @@ shinyServerRun = function(input, output, session, context) {
   output$body = renderUI({
     mainPanel(
         checkboxInput("affine", "Affine normalization", value = TRUE),
-        checkboxInput("refset", "Use a reference set", value = FALSE),
+        checkboxInput("refset", "Use reference data", value = FALSE),
         conditionalPanel(condition = 'input.refset',
-            fileInput("reffile", "Select file with reference set")
+            fileInput("reffile", "Browse ...")
         ),
-        actionButton("start", "Run VSN"),
+        actionButton("start", "Run"),
         verbatimTextOutput("status")
       )
   })
 
   getPropertiesAsMapReactive = context$getPropertiesAsMap()
   getRunFolderReactive = context$getRunFolder()
+  getStepFolderReactive = context$getFolder()
   getDataReactive = context$getData()
 
-  observe({
+  refFile = NULL
 
-    getPropertiesAsMapReactive = context$getPropertiesAsMap()
+  observe({
     getPropertiesAsMap=getPropertiesAsMapReactive$value
     if (is.null(getPropertiesAsMap)) return()
     propertiesAsMap = getPropertiesAsMap()
@@ -39,39 +40,48 @@ shinyServerRun = function(input, output, session, context) {
     getData=getDataReactive$value
     if (is.null(getData)) return()
 
-    getFolder = getRunFolderReactive$value
-    if(is.null(getFolder)) return()
+    getRunFolder = getRunFolderReactive$value
+    if(is.null(getRunFolder)) return()
+
+    getStepFolder = getStepFolderReactive$value
+    if(is.null(getStepFolder)) return()
 
     bndata = getData()
     df = bndata$data
 
     output$status = renderText({
+      isolate({
+        bRef = input$refset
+      })
       if(input$start >0){
         showNotification(ui = "Running VSN ...", type = "message", closeButton = FALSE, duration = NULL)
         if (bndata$hasColors){
           grouping = droplevels(interaction(df[bndata$colorColumnNames]))
         } else {
-          grouping = "none"
+          grouping = "Main"
         }
         df = data.frame(df, grp = grouping)
-        if(!input$refset){
-          #vsnResult = df %>% group_by(grp) %>% do(vsn0(., normalization = input$affine))
-          vsnResult = ddply(df, ~ grp, .fun = vsn0, normalization = input$affine)
-          #hdf = vsnResult %>% group_by(grp) %>% do(vsnh(.))
-          hdf = ddply(vsnResult, ~ grp, .fun = vsnh)
-        } else{
-          if(is.null(input$reffile)) return()
-          refFile = input$reffile
-          load(refFile$datapath)
-          refdf = aCube
-          vsnResult = df %>% group_by(grp) %>% do(vsnr(., refdf, normalization = input$affine))
-
-          hdf = vsnResult %>% group_by(grp) %>% do(vsnh(.))
-        }
-        save(file = file.path(getFolder(),"runData.RData"), vsnResult)
-
+        isolate({
+          if(!bRef){
+            vsnResult = df %>% group_by(grp) %>% do(vsn0(., normalization = input$affine))
+            hdf = vsnResult %>% group_by(grp) %>% do(vsnh(.))
+            reslist = list(vsnResult = vsnResult)
+          }else{
+            refdf = getRefData()
+            if(!is.null(refdf)){
+              vsnResult = df %>% group_by(grp) %>% do(vsnr(., refdf, normalization = input$affine))
+              hdf = vsnResult %>% group_by(grp) %>% do(vsnh(.))
+              reslist = list(vsnResult = vsnResult, refdf = refdf, refFileName = input$reffile$name)
+            } else {
+              stop("Please select a file with a reference set.")
+            }
+          }
+        })
+        save(file = file.path(getRunFolder(),"runData.RData"), reslist)
         hdf = hdf[,-1]
         hdf = hdf[!is.na(hdf$Hvsn),]
+        hdf$rowSeq = as.double(hdf$rowSeq)
+        hdf$colSeq = as.double(hdf$colSeq)
         meta.hdf = data.frame(labelDescription = c("rowSeq", "colSeq", "Hvsn"),
                               groupingType = c("rowSeq", "colSeq", "QuantitationType"))
         result = AnnotatedData$new(data = hdf, metadata = meta.hdf)
@@ -80,26 +90,31 @@ shinyServerRun = function(input, output, session, context) {
       } else {
         return(".")
       }
-
     })
 
-    # observeEvent(input$start, {
-    #   if(is.null(input$start)) return()
-    #   if ( (propertiesAsMap$Interactive == "No") | (input$start > 0) ){
-    #     val = go()
-    #     go(val + 1)
-    #   }
-    # }, ignoreNULL = FALSE)
+    getRefData = reactive({
+      refFile = input$reffile
+      if(is.null(refFile)){
+        return(NULL)
+      } else {
+       load(refFile$datapath)
+       refdf = aCube
+       return(refdf)
+      }
+    })
+
   })
 }
 
+#' @export
 shinyServerShowResults = function(input, output, session, context){
   getFolderReactive = context$getRunFolder()
 
   output$body = renderUI({
     mainPanel(
       selectInput("group", "Show meanSdPlot for", choices = ""),
-      plotOutput("msplot")
+      plotOutput("msplot"),
+      verbatimTextOutput("ref")
     )
   })
 
@@ -107,11 +122,20 @@ shinyServerShowResults = function(input, output, session, context){
     getFolder = getFolderReactive$value
     if (is.null(getFolder)) return()
     load(file.path(getFolder(), "runData.RData"))
+    vsnResult = reslist$vsnResult
     updateSelectInput(session,"group", choices = vsnResult$grp)
 
     output$msplot = renderPlot({
       idx = which(input$group == vsnResult$grp)
       meanSdPlot(vsnResult$vsn[[idx[1]]])
+    })
+
+    output$ref = renderText({
+      if(!is.null(reslist$refFileName)){
+        return(reslist$refFileName)
+      } else {
+        return("No reference file used.")
+      }
     })
   })
 }
